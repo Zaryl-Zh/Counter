45319daa7918d52a468782847221fb4470c702c2
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2021-10-29 17:39:02 +0600

    'HW-34'

---
+++ src/Components/Counter.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react"
+import './Counter.css'
+
+const Counter = () => {
+
+     const [count, setCount] = useState(0)
+
+
+     const valueChangeHandlerPlus = () => {
+        setCount((prevCount) => prevCount+1)
+    }
+
+    const valueChangeHandlerMinus = () => {
+        setCount((prevCount) => prevCount-1)
+    }
+
+    return (
+        <div className="counter">
+          <button type='button' className='btn' onClick={valueChangeHandlerPlus}>+</button> 
+         <h2 className='count'>{count}</h2>
+         <button type='button' className='btn' onClick={valueChangeHandlerMinus}>-</button>   
+        </div>
+    )
+}
+
+export default Counter;
